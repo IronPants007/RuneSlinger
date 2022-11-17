189ce33bd3025c7ed6f70d6fcf90959258197a3d
Afevans0001 <114764886+Afevans0001@users.noreply.github.com>
2022-11-17 17:31:34 -0600

    Update PauseMenu.cs

--- PauseMenu/PauseMenu.cs
+++ PauseMenu/PauseMenu.cs
@@ -43,12 +43,6 @@ public class PauseMenu : MonoBehaviour
     }
     public void QuitGame ()
     {
-        Debug.Log("Quitting game...");
         Application.Quit();
-    }
-    public void Options ()
-    {
-
-        Debug.Log("Loading...");
     }
 }
